6bce9b4c1a939dd1c0f5bf7b6bc99765dc76c8c5
Amar harwani <amar.harwaniQsigma-systems.com>
2016-03-04 12:41:33 +0530

    Update Langurage

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(middleware.logger);
 
 // below middleware.requireAuthentication is added to this particular route , and  will call string private route hit
 app.get('/about' ,middleware.requireAuthentication, function (req, res) {
- 	res.send('about us')
+ 	res.send('about us !')
  });
 
 app.use(express.static(__dirname + '/public'));
